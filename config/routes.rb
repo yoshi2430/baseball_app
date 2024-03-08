3959e93b22de8a6ce1441ecacebaed4af07c3835
yoshi2430 <158545935+yoshi2430@users.noreply.github.com>
2024-03-08 14:08:59 +0900

    デバイス導入

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  resources :players
+
+  root to: 'players#index'
+end
---
+++ db/migrate/20240308021836_create_players.rb
@@ -0,0 +1,13 @@
+class CreatePlayers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :players do |t|
+      t.string :name, null: false
+      t.integer :position_id, null: false
+      t.date :birthday, null: false
+      t.float :height, null: false
+      t.float :weight, null: false
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
